67e3f480ad64e9ad1719be7cd6ad7652eebe84e4
rross <rross>
2004-09-13 20:07:52 +0000

    Formatting.

--- examples/life/mlife2d.c
+++ examples/life/mlife2d.c
@@ -36,8 +36,8 @@ int main(int argc, char *argv[])
     /* Print the total time taken */
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0)
-	printf("[%d] Life finished in %lf secs of calculation\n",
-	       rank, time/100.0);
+        printf("[%d] Life finished in %lf secs of calculation\n",
+               rank, time/100.0);
 
     MLIFEIO_Finalize();
     MPI_Finalize();
@@ -52,14 +52,16 @@ double life(int rows, int cols, int ntimes, MPI_Comm comm)
     int      next, prev, left, right;
     int      i, j, k;
     int    **matrix, **temp, **addr;
-    int      *matrixData, *tempData;
+    int     *matrixData, *tempData;
     double   slavetime, totaltime, starttime;
-    int      LRows, LCols;     /* Number of rows and columns in local mesh
-				  (without halo) */
-    int      GFirstRow, GFirstCol; /* Index of the first row and column
-				      of this mesh in the global mesh, 
-				      relative to the [1][1] entry of the 
-				      local mesh */
+
+    /* count of local rows/columns excluding halo */
+    int      LRows, LCols;
+
+    /* index of first row and col of local mesh in the global
+     * mesh relative to the [1][1] entry of local mesh
+     */
+    int      GFirstRow, GFirstCol;
 
     MPI_Comm_size(comm, &nprocs);
     MPI_Comm_rank(comm, &rank);
@@ -67,80 +69,80 @@ double life(int rows, int cols, int ntimes, MPI_Comm comm)
     /* set neighbors and determine my part of the matrix,
      * block-block distribution
      */
-    MLIFE_MeshDecomp(rank, nprocs, 
-		     rows, cols,
-		     &left, &right, &prev, &next, 
-		     &LRows, &LCols, &GFirstRow, &GFirstCol);
-
-    /* allocate the memory dynamically for the matrix */
+    MLIFE_MeshDecomp(rank, nprocs, rows, cols,
+                     &left, &right, &prev, &next, 
+                     &LRows, &LCols, &GFirstRow, &GFirstCol);
 
     /* allocate memory for the matrix using single blocks */
     matrix     = (int **) malloc((LRows+2) * sizeof(int *));
     temp       = (int **) malloc((LRows+2) * sizeof(int *));
-    matrixData = (int *) malloc((LRows+2)*(LCols+2) * sizeof(int));
-    tempData   = (int *) malloc((LRows+2)*(LCols+2) * sizeof(int));
+    matrixData = (int *) malloc((LRows+2)*(LCols+2)*sizeof(int));
+    tempData   = (int *) malloc((LRows+2)*(LCols+2)*sizeof(int));
 
     /* set up pointers for convenience */
     matrix[0] = matrixData;
     temp[0]   = tempData;
     for (i = 1; i < LRows+2; i++) {
-	matrix[i] = matrix[i-1] + LCols + 2;
-	temp[i]   = temp[i-1]   + LCols + 2;
+        matrix[i] = matrix[i-1] + LCols + 2;
+        temp[i]   = temp[i-1]   + LCols + 2;
     }
 
     /* initialize the boundaries of the life matrix */
     for (j = 0; j < LCols+2; j++) {
-	matrix[0][j] = matrix[LRows+1][j] = temp[0][j]
-	             = temp[LRows+1][j] = DIES;
+        matrix[0][j] = matrix[LRows+1][j] = temp[0][j]
+                     = temp[LRows+1][j] = DIES;
     }
     for (i = 0; i < LRows+2; i++) {
-	matrix[i][0] = matrix[i][LCols+1] = temp[i][0]
-	             = temp[i][LCols+1] = DIES;
+        matrix[i][0] = matrix[i][LCols+1] = temp[i][0]
+                     = temp[i][LCols+1] = DIES;
     }
 
     /* Initialize the life matrix */
     for (i = 1; i <= LRows; i++)  {
-	srand48((long)(1000^(i + GFirstRow-1)));
-	/* advance to the random number generator to the first *owned* cell */
-	for (j=1; j<GFirstCol; j++) {    
-	    (void)drand48();
-	}
+        srand48((long)(1000^(i + GFirstRow-1)));
+        /* advance to the random number generator to the
+	 * first *owned* cell
+	 */
+        for (j=1; j<GFirstCol; j++) {    
+            (void)drand48();
+        }
 
-	for (j = 1; j<= LCols; j++)
-	    if (drand48() > 0.5) matrix[i][j] = BORN;
-	    else                 matrix[i][j] = DIES;
+        for (j = 1; j<= LCols; j++)
+            if (drand48() > 0.5) matrix[i][j] = BORN;
+            else                 matrix[i][j] = DIES;
     }
 
-    MLIFE_exchange_init(comm, &matrix[0][0], &temp[0][0], rows, cols, 
-			LRows, LCols, prev, next, left, right);
+    MLIFE_exchange_init(comm, &matrix[0][0], &temp[0][0], rows,
+			cols, LRows, LCols, prev, next, left,
+			right);
 
     /* use portable MPI function for timing */
     starttime = MPI_Wtime();
 
     for (k = 0; k < ntimes; k++)
     {
-	MLIFE_exchange(matrix, LRows, LCols);
+        MLIFE_exchange(matrix, LRows, LCols);
 
-	/* calculate new state for all non-boundary elements */
-	for (i = 1; i <= LRows; i++) {
-	    for (j = 1; j < LCols+1; j++) {
-		temp[i][j] = MLIFE_nextstate(matrix, i, j);
-	    }
-	}
+        /* calculate new state for all non-boundary elements */
+        for (i = 1; i <= LRows; i++) {
+            for (j = 1; j < LCols+1; j++) {
+                temp[i][j] = MLIFE_nextstate(matrix, i, j);
+            }
+        }
 
-	/* swap the matrices */
-	addr   = matrix;
-	matrix = temp;
-	temp   = addr;
+        /* swap the matrices */
+        addr   = matrix;
+        matrix = temp;
+        temp   = addr;
 
-	MLIFEIO_Checkpoint(opt_prefix, matrix, rows, cols, 
-			   k, MPI_INFO_NULL);
+        MLIFEIO_Checkpoint(opt_prefix, matrix, rows, cols, 
+                           k, MPI_INFO_NULL);
     }
 
     /* Return the average time taken/processor */
     slavetime = MPI_Wtime() - starttime;
     MPI_Reduce(&slavetime, &totaltime, 1, MPI_DOUBLE, MPI_SUM, 0,
-	       comm);
+               comm);
 
     MLIFE_exchange_finalize();
     free(matrix);
@@ -152,66 +154,72 @@ double life(int rows, int cols, int ntimes, MPI_Comm comm)
 }
 
 
-/* Compute coordinates of this patch, given the rank and size of the
-   global mesh.  Also return the neighbors */
-void MLIFE_MeshDecomp(int rank, int nprocs, 
-		      int GRows, int GCols,
-		      int *leftP, int *rightP, int *topP, int *bottomP, 
-		      int *LRowsP, int *LColsP, 
-		      int *GFirstRowP, int *GFirstColP)
+/* MLIFE_MeshDecomp
+ *
+ * Compute coordinates of this patch, given the rank and size of
+ * the global mesh.  Also return the neighbors.
+ */
+void MLIFE_MeshDecomp(int rank, int nprocs, int GRows, int GCols,
+                      int *leftP, int *rightP,
+		      int *topP, int *bottomP, 
+                      int *LRowsP, int *LColsP, 
+                      int *GFirstRowP, int *GFirstColP)
 {
     int dims[2];
     int top, bottom, left, right;
     int prow, pcol;
     int firstrow, lastrow, firstcol, lastcol;
 
-    /* Form the decomposition */
     dims[0] = opt_prows;
     dims[1] = opt_pcols;
 
     MPI_Dims_create(nprocs, 2, dims);
 
-    /* Compute the cartesian coords of this process; number across
-       rows changing column by 1 changes rank by 1) */
+    /* compute the cartesian coords of this process; number across
+     * rows changing column by 1 changes rank by 1)
+     */
     prow = rank / dims[1];
     pcol = rank % dims[1];
     
     /* Compute the neighbors */
     left = right = top = bottom = MPI_PROC_NULL;
     if (prow > 0) {
-	top = rank - dims[1];
+        top = rank - dims[1];
     }
     if (pcol > 0) {
-	left = rank - 1;
+        left = rank - 1;
     }
     if (prow < dims[0]-1) {
-	bottom = rank + dims[1];
+        bottom = rank + dims[1];
     }
     if (pcol < dims[1]-1) {
-	right = rank + 1;
+        right = rank + 1;
     }
     if (leftP) {
-	/* Allow a NULL for leftP to suppress all of the neighbor 
-	   information */
-	*leftP = left; *rightP = right; *topP = top; *bottomP = bottom;
+        /* allow a NULL for leftP to suppress all of the neighbor
+         * information
+	 */
+        *leftP = left; *rightP = right;
+	*topP = top; *bottomP = bottom;
     }
     
-    /* Compute the decomposition of the global mesh.
-       These are for the "active" part of the mesh, and range from 1 to GRows
-       by 1 to GCols */
+    /* compute the decomposition of the global mesh.
+     * these are for the "active" part of the mesh, and range from
+     * 1 to GRows by 1 to GCols
+     */
     firstcol = 1 + pcol * (GCols / dims[1]);
     firstrow = 1 + prow * (GRows / dims[0]);
     if (pcol == dims[1] - 1) {
-	lastcol = GCols;
+        lastcol = GCols;
     }
     else {
-	lastcol  = 1 + (pcol + 1) * (GCols / dims[1]) - 1;
+        lastcol  = 1 + (pcol + 1) * (GCols / dims[1]) - 1;
     }
     if (prow == dims[0] - 1) {
-	lastrow = GRows;
+        lastrow = GRows;
     }
     else {
-	lastrow = 1 + (prow + 1) * (GRows / dims[0]) - 1;
+        lastrow = 1 + (prow + 1) * (GRows / dims[0]) - 1;
     }
 
     *LRowsP     = lastrow - firstrow + 1;
@@ -222,16 +230,16 @@ void MLIFE_MeshDecomp(int rank, int nprocs,
 
 
 static int MLIFE_nextstate(int **matrix,
-			   int row,
-			   int col)
+                           int row,
+                           int col)
 {
     int sum;
 
     /* add values of all eight neighbors */
     sum = matrix[row-1][col-1] + matrix[row-1][col] +
-	  matrix[row-1][col+1] + matrix[row][col-1] +
-	  matrix[row][col+1] + matrix[row+1][col-1] +
-	  matrix[row+1][col] + matrix[row+1][col+1];
+          matrix[row-1][col+1] + matrix[row][col-1] +
+          matrix[row][col+1] + matrix[row+1][col-1] +
+          matrix[row+1][col] + matrix[row+1][col+1];
 
     if (sum < 2 || sum > 3) return DIES;
     else if (sum == 3)      return BORN;
